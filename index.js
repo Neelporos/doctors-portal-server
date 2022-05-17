db1b9cfba26878e7058cd542c3b10f57e3633966
NeelPoros <sawkotalimahmud@gmail.com>
2022-05-17 10:24:01 -0700

    Updating User

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ async function run() {
     await client.connect();
     const serviceCollection = client.db("doctors_portal").collection("services");
     const bookingCollection = client.db("doctors_portal").collection("booking");
+    const userCollection = client.db("doctors_portal").collection("users");
     
     // Get All Service
     app.get('/service', async (req, res) => {
@@ -26,6 +27,19 @@ async function run() {
       const cursor = serviceCollection.find(query);
       const services = await cursor.toArray();
       res.send(services);
+    });
+
+    // User Update Section
+    app.put('/user/:email', async (req, res) =>{
+      const email = req.params.email;
+      const user = req.body;
+      const filter = {email: email};
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: user,
+      };
+      const result = await userCollection.updateOne(filter, updateDoc, options);
+      res.send(result);
     });
 
     // Get Booking Section
